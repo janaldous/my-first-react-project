9225d733e77a4d24aa3597772dbf5b96a8743663
janaldous <jat.torres@gmail.com>
2019-09-06 20:31:32 +0800

    added tests

--- src/test/components/Link.react.test.tsx
+++ src/test/components/Link.react.test.tsx
@@ -10,8 +10,10 @@ test('Link changes the class when hovered', () => {
     expect(1).toBe(1);
 });
 
-test('checkboxwithlabel changes the text after click', () => {
-    let toggle = () => console.log("toggled");
-    const sidebar = shallow(<SideBar isOpen={false} toggle={toggle}></SideBar>)
-    expect(sidebar).toBeTruthy();
-})
+describe('Sidebar', () => {
+    it('opens if X is clicked when closed', () => {
+        let toggle = () => console.log("toggled");
+        const sidebar = shallow(<SideBar isOpen={false} toggle={toggle}></SideBar>);
+        expect(sidebar).toBeTruthy();
+    });
+});
